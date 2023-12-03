fbddad22d8b713f879b8f8e90d60653e8767b241
Stanley <offorstanley4real@gmail.com>
2023-12-03 17:01:15 +0100

    first commit

---
+++ Desktop/JOBS/Framer/framer-site/src/App.tsx
@@ -0,0 +1,15 @@
+import Body from "./pages/Body";
+import Footer from "./pages/Footer";
+import Navbar from "./pages/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Body />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
